cfdf562c5ac9f0d08cea5bccd7704550e39af74e
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2021-01-23 20:49:45 -0500

    Initial Commit

---
+++ TestCapture/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestCapture
+//
+//  Created by ka37 on 1/23/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
