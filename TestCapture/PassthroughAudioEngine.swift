d499d08a0c41c44395f2a2ab7c7ad3c540c078ba
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2021-02-13 21:10:30 -0500

    Now there's output, trying to figure out what the difference is.

--- TestCapture/PassthroughAudioEngine.swift
+++ TestCapture/PassthroughAudioEngine.swift
@@ -11,17 +11,63 @@ import AVFoundation
 class PassthroughAudioEngine {
     private let inputEngine = AVAudioEngine()
     private let outputEngine = AVAudioEngine()
-    
-    var masterBuffer: AVAudioPCMBuffer;
     var outputFormat: AVAudioFormat;
 
     init() {
         outputFormat = outputEngine.outputNode.outputFormat(forBus: 0)
         
-        masterBuffer = AVAudioPCMBuffer(
-            pcmFormat: outputFormat, frameCapacity: 44100
-        )!
+//        masterBuffer = AVAudioPCMBuffer(
+//            pcmFormat: outputFormat, frameCapacity: 44100
+//        )!
     }
+    
+    func prepare() {
+        let frequency: Float = 440
+        let amplitude: Float = 0.5
+
+        let twoPi = 2 * Float.pi
+
+        let mainMixer = outputEngine.mainMixerNode
+        let output = outputEngine.outputNode
+        let outputFormat = output.inputFormat(forBus: 0)
+        let sampleRate = Float(outputFormat.sampleRate)
+        // Use output format for input but reduce channel count to 1
+
+        var currentPhase: Float = 0
+        // The interval by which we advance the phase each frame.
+        let phaseIncrement = (twoPi / sampleRate) * frequency
+
+        let srcNode = AVAudioSourceNode { _, _, frameCount, audioBufferList -> OSStatus in
+            let ablPointer = UnsafeMutableAudioBufferListPointer(audioBufferList)
+            for frame in 0..<Int(frameCount) {
+                // Advance the phase for the next frame.
+                currentPhase += phaseIncrement
+                if currentPhase >= twoPi {
+                    currentPhase -= twoPi
+                }
+                if currentPhase < 0.0 {
+                    currentPhase += twoPi
+                }
+                // Set the same value on all channels (due to the inputFormat we have only 1 channel though).
+                for (i, buffer) in ablPointer.enumerated() {
+                    let buf: UnsafeMutableBufferPointer<Float> = UnsafeMutableBufferPointer(buffer)
+                    buf[frame] = sin(Float(frame) * phaseIncrement * Float(i + 1)) * amplitude
+                }
+            }
+            return noErr
+        }
+
+        outputEngine.attach(srcNode)
+
+        outputEngine.connect(srcNode, to: mainMixer, format: outputFormat)
+        outputEngine.connect(mainMixer, to: output, format: outputFormat)
+        mainMixer.outputVolume = 0.5
+    }
+
+/*
+class PassthroughAudioEngine {
+    
+    var masterBuffer: AVAudioPCMBuffer;
     
     fileprivate func prepareInputEngine() {
         inputEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: inputEngine.inputNode.inputFormat(forBus: 0)) {
@@ -79,10 +125,9 @@ class PassthroughAudioEngine {
 
         outputEngine.prepare()
     }
-    
+*/
     func start() throws {
 //        try inputEngine.start()
         try outputEngine.start()
     }
 }
-
