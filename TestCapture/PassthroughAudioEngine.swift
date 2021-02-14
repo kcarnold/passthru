20741a9df2e7320c54c543f7fce14a6a75730e8b
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2021-02-13 21:05:54 -0500

    Attempting to make an output engine work.

---
+++ TestCapture/PassthroughAudioEngine.swift
@@ -0,0 +1,88 @@
+//
+//  AudioSource.swift
+//  TestCapture
+//
+//  Created by ka37 on 1/23/21.
+//
+
+import Foundation
+import AVFoundation
+
+class PassthroughAudioEngine {
+    private let inputEngine = AVAudioEngine()
+    private let outputEngine = AVAudioEngine()
+    
+    var masterBuffer: AVAudioPCMBuffer;
+    var outputFormat: AVAudioFormat;
+
+    init() {
+        outputFormat = outputEngine.outputNode.outputFormat(forBus: 0)
+        
+        masterBuffer = AVAudioPCMBuffer(
+            pcmFormat: outputFormat, frameCapacity: 44100
+        )!
+    }
+    
+    fileprivate func prepareInputEngine() {
+        inputEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: inputEngine.inputNode.inputFormat(forBus: 0)) {
+            buffer, when in
+            let bufferSize = buffer.frameLength
+            var power = Float(0.0);
+            let channelCount = Int(buffer.format.channelCount);
+            
+            for channel in 0..<channelCount {
+                let srcData = buffer.floatChannelData![channel]
+                let tgtData = self.masterBuffer.floatChannelData![channel]
+                for frame in 0..<Int(buffer.frameLength) {
+                    tgtData[frame] = srcData[frame]
+                    power += pow(srcData[frame], 2)
+                }
+            }
+            self.masterBuffer.frameLength = buffer.frameLength
+            print("Input buffer! \(channelCount) \(bufferSize) \(when.isHostTimeValid) \(power / Float(bufferSize))")
+        }
+        
+        inputEngine.prepare()
+    }
+    
+    func prepare() {
+        
+        //prepareInputEngine()
+        
+        let phaseIncrement = 2 * Float.pi / Float(outputFormat.sampleRate) * 440;
+        
+        let playerNode = AVAudioSourceNode { (silence, timeStamp, frameCount, audioBufferList) -> OSStatus in
+            let audioBufferListPtr = UnsafeMutableAudioBufferListPointer(audioBufferList)
+            print("Output buffer! \(frameCount) \(audioBufferListPtr.count)")
+
+//            let masterBufferMut = UnsafeMutableAudioBufferListPointer(self.masterBuffer.mutableAudioBufferList)
+
+            for (i, outBufferChannel) in audioBufferListPtr.enumerated() {
+//            for (masterBufferChannel, outBufferChannel) in zip(masterBufferMut, audioBufferListPtr) {
+//                let masterBufferPtr = UnsafeMutableBufferPointer<Float>(masterBufferChannel)
+                let outBufferPtr = UnsafeMutableBufferPointer<Float>(outBufferChannel)
+                for frame in 0..<Int(frameCount) {
+                    var sample = sin(Float(frame) * phaseIncrement)//Float(0);
+//                    if (frame < self.masterBuffer.frameLength) {
+//                        sample = masterBufferPtr[frame]
+//                    }
+                    outBufferPtr[frame] = sample
+                }
+            }
+            return noErr
+        }
+
+        outputEngine.attach(playerNode)
+        outputEngine.connect(playerNode, to: outputEngine.mainMixerNode, format: outputFormat)
+        outputEngine.connect(outputEngine.mainMixerNode, to: outputEngine.outputNode, format: outputFormat)
+        outputEngine.mainMixerNode.outputVolume = 1.0
+
+        outputEngine.prepare()
+    }
+    
+    func start() throws {
+//        try inputEngine.start()
+        try outputEngine.start()
+    }
+}
+
--- TestCapture/ViewController.swift
+++ TestCapture/ViewController.swift
@@ -9,30 +9,11 @@ import Cocoa
 import AVFoundation
 
 class ViewController: NSViewController, AVCaptureAudioDataOutputSampleBufferDelegate {
-    private let engine = AVAudioEngine()
-
+    private let engine = PassthroughAudioEngine()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        engine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: engine.inputNode.inputFormat(forBus: 0)) {
-            buffer, when in
-            let bufferSize = buffer.frameLength
-            var power = Float(0.0);
-            let channelCount = Int(buffer.format.channelCount);
-            
-            
-            for channel in 0..<channelCount {
-                let srcData = buffer.floatChannelData![channel]
-                //let tgtData = masterBuffer?.floatChannelData![channel]
-                for frame in 0..<Int(buffer.frameLength) {
-                    //tgtData![frame] = srcData[frame]
-                    power += pow(srcData[frame], 2)
-                }
-            }
-            //masterBuffer?.frameLength = buffer.frameLength
-            print("Input buffer! \(channelCount) \(bufferSize) \(when.isHostTimeValid) \(power / Float(bufferSize))")
-        }
-        
         engine.prepare()
         
         do {
